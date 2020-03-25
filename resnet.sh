8057431419ece389db86c950a54fba7ae3478a24
woosung <fredrickang@naver.com>
2020-03-25 19:27:16 +0900

    sync

---
+++ resnet.sh
@@ -0,0 +1,4 @@
+#./darknet detector periodic -models models.list -weights weights.list -data data.list -res_cfg resource.list -period 220 -process_num 4 -filename coco/100.part
+#./darknet classifier predict ./cfg/imagenet1k.data ./extraction.cfg ./extraction.weights -res_cfg res_cfg.part -process_num 1 ./data/eagle.jpg
+sudo ./darknet -task ./task/resnet18.list -period 300
+
